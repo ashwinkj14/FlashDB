251f62ef5489d87e140110a2c9dcb9228358ec9f
ashwinkj14 <ashwinkj97@gmail.com>
2024-10-22 17:53:13 -0700

    Config: sl4j for logging

--- src/main/java/org/ashwin/Application.java
+++
@@ -1,5 +0,0 @@
-package org.ashwin;
-
-public class Application {
-    public static void main(String[] args) {}
-}
---
+++ src/main/java/org/ashwin/flash/Application.java
@@ -0,0 +1,12 @@
+package org.ashwin.flash;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Application {
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        logger.info("Application started");
+    }
+}
