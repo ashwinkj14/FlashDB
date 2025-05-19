4a32df9f72280d178ec7a460d0c1cffa58afb160
ashwinkj14 <ashwinkj97@gmail.com>
2025-05-19 01:02:57 -0700

    Feat: add custom interceptor and control the number of threads

--- src/main/java/org/ashwin/flash/Application.java
+++ src/main/java/org/ashwin/flash/Application.java
@@ -1,10 +1,16 @@
 package org.ashwin.flash;
 
+import io.grpc.Metadata;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
+import io.grpc.ServerCall;
+import io.grpc.ServerCallHandler;
+import io.grpc.ServerInterceptor;
 import org.ashwin.flash.grpc.FlashDBServiceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.Executors;
 
 public class Application {
     private static final Logger logger = LoggerFactory.getLogger(Application.class);
@@ -15,6 +21,16 @@ public class Application {
     private void start() throws Exception {
         server = ServerBuilder.forPort(PORT)
                 .addService(new FlashDBServiceImpl())
+                .intercept(new ServerInterceptor() {
+                    @Override
+                    public <ReqT, RespT> ServerCall.Listener<ReqT> interceptCall(
+                            ServerCall<ReqT, RespT> call,
+                            Metadata headers,
+                            ServerCallHandler<ReqT, RespT> next) {
+                        return next.startCall(call, headers);
+                    }
+                })
+                .executor(Executors.newFixedThreadPool(10))
                 .build()
                 .start();
 
