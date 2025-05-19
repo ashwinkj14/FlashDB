5a72420ef0758dc23e6c0b64a30e25305f3725cb
ashwinkj14 <ashwinkj97@gmail.com>
2025-05-18 21:33:26 -0700

    Test: modify database test

--- src/test/java/org/ashwin/flash/database/engine/DatabaseTest.java
+++ src/test/java/org/ashwin/flash/database/engine/DatabaseTest.java
@@ -1,7 +1,11 @@
 package org.ashwin.flash.database.engine;
 
+import org.ashwin.flash.database.service.StorageService;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.nio.charset.StandardCharsets;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,33 +14,38 @@ public class DatabaseTest {
 
     @BeforeEach
     public void setup(){
-        database = new Database("test");
+        database = new Database("testDB");
+    }
+
+    @AfterAll
+    public static void cleanup(){
+        StorageService.getInstance().deleteDatabase("testDB");
     }
 
     @Test
     public void testDatabaseCreation(){
-        assertEquals("test", database.getDatabaseName(), "Database 'test' should be created");
+        assertEquals("testDB", database.getDatabaseName(), "Database 'test' should be created");
     }
 
     @Test
     public void testInsertion(){
-        database.put("key1", "value1");
-        String value = database.get("key1");
-        assertEquals("value1", value, "The value for 'key1' should be 'value1'");
+        database.put("key1", "value1".getBytes());
+        byte[] value = database.get("key1");
+        assertEquals("value1", new String(value, StandardCharsets.UTF_8), "The value for 'key1' should be 'value1'");
     }
 
     @Test
-    public void testNonExistentKey(){
-        String value = database.get("key2");
+    public void test_NonExistentKey(){
+        byte[] value = database.get("key2");
         assertNull(value, "Querying a non-existent key should return null");
     }
 
     @Test
-    public void testUpdate(){
-        database.put("key1", "value1");
-        database.put("key1", "value2");
-        String value = database.get("key1");
-        assertEquals("value2", value, "The value for 'key1' should be 'value2'");
+    public void test_Update(){
+        database.put("key1", "value1".getBytes());
+        database.put("key1", "value2".getBytes());
+        byte[] value = database.get("key1");
+        assertEquals("value2", new String(value, StandardCharsets.UTF_8), "The value for 'key1' should be 'value2'");
     }
 
 
