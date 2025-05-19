3f27beae17f9f02c813216295d43cbf3b3ed28c2
ashwinkj14 <ashwinkj97@gmail.com>
2025-05-18 21:36:08 -0700

    Feat: Create a service for handling gRPC requests

---
+++ src/main/java/org/ashwin/flash/grpc/FlashDBServiceImpl.java
@@ -0,0 +1,87 @@
+package org.ashwin.flash.grpc;
+
+import org.ashwin.flash.database.engine.Database;
+import org.ashwin.flash.database.service.StorageService;
+import org.ashwin.flash.proto.FlashDBServiceGrpc;
+import org.ashwin.flash.proto.GetRequest;
+import org.ashwin.flash.proto.GetResponse;
+import org.ashwin.flash.proto.SetRequest;
+import org.ashwin.flash.proto.SetResponse;
+import org.ashwin.flash.proto.DeleteRequest;
+import org.ashwin.flash.proto.DeleteResponse;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+
+@GrpcService
+public class FlashDBServiceImpl extends FlashDBServiceGrpc.FlashDBServiceImplBase {
+
+    @Override
+    public void get(GetRequest request, StreamObserver<GetResponse> responseObserver) {
+        String key = request.getKey();
+        String databaseName = request.getDatabase();
+        Database database = StorageService.getInstance().getDatabase(databaseName);
+        byte[] value = database.get(key);
+
+        GetResponse response;
+        if (value != null) {
+            response = GetResponse.newBuilder()
+                .setFound(true)
+                .setValue(com.google.protobuf.ByteString.copyFrom(value))
+                .build();
+        } else {
+            response = GetResponse.newBuilder()
+                .setFound(false)
+                .build();
+        }
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void set(SetRequest request, StreamObserver<SetResponse> responseObserver) {
+        try {
+            String key = request.getKey();
+            String databaseName = request.getDatabase();
+            Database database = StorageService.getInstance().getDatabase(databaseName);
+            byte[] value = request.getValue().toByteArray();
+
+            database.put(key, value);
+
+            SetResponse response = SetResponse.newBuilder()
+                .setSuccess(true)
+                .setMessage("Key-value pair successfully stored")
+                .build();
+
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+        } catch (Exception e) {
+            SetResponse response = SetResponse.newBuilder()
+                .setSuccess(false)
+                .setMessage("Error storing key-value pair: " + e.getMessage())
+                .build();
+
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+        }
+    }
+
+    @Override
+    public void delete(DeleteRequest request, StreamObserver<DeleteResponse> responseObserver) {
+        String key = request.getKey();
+        String databaseName = request.getDatabase();
+        Database database = StorageService.getInstance().getDatabase(databaseName);
+        boolean removed = database.delete(key);
+
+        DeleteResponse response = DeleteResponse.newBuilder()
+            .setSuccess(removed)
+            .setMessage(removed ?
+                "Key successfully deleted" :
+                "Key not found")
+            .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+}
