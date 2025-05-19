2eaf0c425871182127f1e7a6e39a2f1991ccc3ff
ashwinkj14 <ashwinkj97@gmail.com>
2025-05-18 21:30:34 -0700

    Feat: Validate params before performing database operations

--- src/main/java/org/ashwin/flash/database/engine/Database.java
+++ src/main/java/org/ashwin/flash/database/engine/Database.java
@@ -1,29 +1,43 @@
 package org.ashwin.flash.database.engine;
 
 import org.ashwin.flash.config.FlashProperties;
-import org.ashwin.flash.database.service.StorageService;
 
 public class Database {
 
     private final String name;
     private final LSMTree lsmTree;
 
+    private void validateKey(String key) {
+        if (key == null || key.isEmpty()){
+            throw new IllegalArgumentException("Key cannot be null or empty");
+        }
+    }
+
     public Database(String name) {
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("Database name cannot be null or empty");
+        }
         this.name = name;
-        lsmTree = new LSMTree(Integer.parseInt(FlashProperties.MEMTABLE_CAPACITY.getDefaultValue()));
-        StorageService.getInstance().createDatabase(name);
+        lsmTree = new LSMTree(Integer.parseInt(FlashProperties.MEMTABLE_CAPACITY.getDefaultValue()), name);
     }
 
     public String getDatabaseName() {
         return name;
     }
 
-    public String get(String key) {
+    public byte[] get(String key) {
+        validateKey(key);
         return lsmTree.get(key);
     }
 
-    public void put(String key, String value) {
+    public void put(String key, byte[] value) {
+        validateKey(key);
         lsmTree.put(key, value);
+    }
+
+    public boolean delete(String key) {
+        validateKey(key);
+        return lsmTree.delete(key);
     }
 
 
